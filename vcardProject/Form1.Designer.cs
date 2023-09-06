07dd171ae9319785ac2ace78087721e70a7a1466
Ruh Adam <avuclariminicinde@gmail.com>
2023-09-06 20:03:11 +0300

    uptaded
    uptaded

---
+++ vcardProject/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace vcardProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtBussines = new System.Windows.Forms.TextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.converVcard = new System.Windows.Forms.Button();
+            this.addToList = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 477);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(965, 147);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSurname.Location = new System.Drawing.Point(124, 62);
+            this.txtSurname.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(296, 30);
+            this.txtSurname.TabIndex = 2;
+            this.txtSurname.Text = "Gökçek";
+            this.txtSurname.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPhone.Location = new System.Drawing.Point(124, 100);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(296, 30);
+            this.txtPhone.TabIndex = 3;
+            this.txtPhone.Text = "5533432506";
+            this.txtPhone.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtName.Location = new System.Drawing.Point(124, 24);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(296, 30);
+            this.txtName.TabIndex = 1;
+            this.txtName.Text = "Alperen";
+            this.txtName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtBussines
+            // 
+            this.txtBussines.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtBussines.Location = new System.Drawing.Point(124, 138);
+            this.txtBussines.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBussines.Name = "txtBussines";
+            this.txtBussines.Size = new System.Drawing.Size(296, 30);
+            this.txtBussines.TabIndex = 4;
+            this.txtBussines.Text = "3125478547";
+            this.txtBussines.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtMail
+            // 
+            this.txtMail.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMail.Location = new System.Drawing.Point(124, 176);
+            this.txtMail.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(296, 30);
+            this.txtMail.TabIndex = 5;
+            this.txtMail.Text = "avuclariminicinde@gmail.com";
+            this.txtMail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(86, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(61, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Soyad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(13, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Cep Telefonu:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(26, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "İş Telefonu:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(76, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 18);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Mail:";
+            this.label5.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.txtMail);
+            this.groupBox1.Controls.Add(this.txtBussines);
+            this.groupBox1.Controls.Add(this.txtPhone);
+            this.groupBox1.Controls.Add(this.txtSurname);
+            this.groupBox1.Font = new System.Drawing.Font("Georgia", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(21, 165);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(469, 230);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kişisel Bilgiler";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.deleteButton);
+            this.groupBox2.Controls.Add(this.converVcard);
+            this.groupBox2.Controls.Add(this.addToList);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.Window;
+            this.groupBox2.Location = new System.Drawing.Point(524, 165);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(397, 230);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "İşlemler";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.SystemColors.Menu;
+            this.deleteButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.deleteButton.Location = new System.Drawing.Point(52, 129);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(281, 35);
+            this.deleteButton.TabIndex = 1;
+            this.deleteButton.Text = "Kişiyi Sil";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // converVcard
+            // 
+            this.converVcard.BackColor = System.Drawing.SystemColors.Menu;
+            this.converVcard.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.converVcard.Location = new System.Drawing.Point(52, 37);
+            this.converVcard.Name = "converVcard";
+            this.converVcard.Size = new System.Drawing.Size(281, 35);
+            this.converVcard.TabIndex = 0;
+            this.converVcard.Text = "Vcard\'a Dönüştür";
+            this.converVcard.UseVisualStyleBackColor = false;
+            this.converVcard.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // addToList
+            // 
+            this.addToList.BackColor = System.Drawing.SystemColors.Menu;
+            this.addToList.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.addToList.Location = new System.Drawing.Point(52, 83);
+            this.addToList.Name = "addToList";
+            this.addToList.Size = new System.Drawing.Size(281, 35);
+            this.addToList.TabIndex = 0;
+            this.addToList.Text = "Listeye Ekle";
+            this.addToList.UseVisualStyleBackColor = false;
+            this.addToList.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(965, 53);
+            this.panel1.TabIndex = 6;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(904, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(858, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "‒";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_2);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(965, 624);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtBussines;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button converVcard;
+        private System.Windows.Forms.Button addToList;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ vcardProject/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace vcardProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class CharacterConverter
+        {
+            public static string ConvertToEnglish(string input)
+            {
+                // Türkçe karakterleri normalize ederek İngilizce karakterlere dönüştür
+                string normalizedString = input.Normalize(NormalizationForm.FormD);
+                StringBuilder result = new StringBuilder();
+
+                foreach (char c in normalizedString)
+                {
+                    // Türkçe karakterlerin İngilizce karakterlere dönüşümünü gerçekleştir
+                    if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                    {
+                        result.Append(c);
+                    }
+                }
+
+                // İngilizce karakterlere dönüştürülmüş metni geri döndür
+                return result.ToString();
+            }
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void clear()
+        {
+
+            txtName.Text = "";
+            txtSurname.Text = "";
+            txtPhone.Text = "";
+            txtBussines.Text = "";
+            txtMail.Text = "";
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        DataTable dt = new DataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dt.Columns.Add("Ad");
+            dt.Columns.Add("Soyad");
+            dt.Columns.Add("Cep Telefonu");
+            dt.Columns.Add("İş Telefonu");
+            dt.Columns.Add("Mail Adresi");
+            dataGridView1.RowHeadersVisible = false; 
+            dataGridView1.DataSource = dt;
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            string name = txtName.Text;
+            string surname = txtSurname.Text;
+            string phone = txtPhone.Text;
+            string bussines = txtBussines.Text;
+            string mail = txtMail.Text;
+
+            name = CharacterConverter.ConvertToEnglish(name);
+            surname = CharacterConverter.ConvertToEnglish(surname);
+            phone = CharacterConverter.ConvertToEnglish(phone);
+            bussines = CharacterConverter.ConvertToEnglish(bussines);
+            mail = CharacterConverter.ConvertToEnglish(mail);
+
+            dt.Rows.Add(name, surname, phone, bussines, mail);
+
+            clear();
+            MessageBox.Show("Kişi başarıyla listeye eklendi.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "vCard Files|*.vcf";
+            saveFileDialog.Title = "Save vCard Files";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName, false, Encoding.UTF8))
+                    {
+                        foreach (DataGridViewRow row in dataGridView1.Rows)
+                        {
+                            // Hücre değerlerini kontrol ederek null olup olmadıklarını kontrol edin.
+                            if (row.Cells["Ad"].Value != null &&
+                                row.Cells["Soyad"].Value != null &&
+                                row.Cells["Cep Telefonu"].Value != null &&
+                                row.Cells["İş Telefonu"].Value != null &&
+                                row.Cells["Mail Adresi"].Value != null)
+                            {
+                                string name = row.Cells["Ad"].Value.ToString();
+                                string surname = row.Cells["Soyad"].Value.ToString();
+                                string phone = row.Cells["Cep Telefonu"].Value.ToString();
+                                string bussines = row.Cells["İş Telefonu"].Value.ToString();
+                                string mail = row.Cells["Mail Adresi"].Value.ToString();
+
+                                sw.WriteLine("BEGIN:VCARD");
+                                sw.WriteLine("VERSION:3.0");
+                                sw.WriteLine($"N:{surname};{name}");
+                                sw.WriteLine($"FN:{name} {surname}");
+                                sw.WriteLine($"TEL;TYPE=CELL:{phone}");
+                                sw.WriteLine($"TEL;TYPE=WORK:{bussines}");
+                                sw.WriteLine($"EMAIL;TYPE=INTERNET:{mail}");
+                                sw.WriteLine("END:VCARD");
+                                sw.WriteLine();
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("Listedeki kişiler Başarıyla Vcard'a dönüştürüldü.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+
+            txtName.Text = dataGridView1.Rows[secilen].Cells[0].Value.ToString();
+            txtSurname.Text = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+            txtPhone.Text = dataGridView1.Rows[secilen].Cells[2].Value.ToString();
+            txtBussines.Text = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            txtMail.Text = dataGridView1.Rows[secilen].Cells[4].Value.ToString();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string nameToDelete = txtName.Text;
+            string surnameToDelete = txtSurname.Text;
+            string phoneToDelete = txtPhone.Text;
+            string bussinesToDelete = txtBussines.Text;
+            string mailToDelete = txtMail.Text;
+            for (int i = dt.Rows.Count - 1; i >= 0; i--)
+            {
+                DataRow row = dt.Rows[i];
+
+                // Verileri karşılaştır ve eşleşen satırı sil
+                if (row["Ad"].ToString() == nameToDelete &&
+                    row["Soyad"].ToString() == surnameToDelete &&
+                    row["Cep Telefonu"].ToString() == phoneToDelete &&
+                    row["İş Telefonu"].ToString() == bussinesToDelete &&
+                    row["Mail Adresi"].ToString() == mailToDelete)
+                {
+                    dt.Rows.Remove(row);
+                }
+            }
+
+            clear();
+            MessageBox.Show("Kişi Başarıyla Silindi.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized; 
+        }
+        int mouseX, mouseY;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Left = MousePosition.X - mouseX;
+            this.Top  = MousePosition.Y - mouseY;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = false; 
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseX = MousePosition.X - this.Left;
+            mouseY = MousePosition.Y - this.Top;
+            timer1.Enabled = true;
+        }
+        
+    }
+}
